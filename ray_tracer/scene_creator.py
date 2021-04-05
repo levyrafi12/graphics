f01e34b742158eeeb373afd7a34d37f0056da4b2
tomer <tomer.amit@bodyvisionmedical.com>
2021-04-05 11:56:59 +0300

    done fixes to parsing script

---
+++ ray_tracer/scene_creator.py
@@ -0,0 +1,5 @@
+from ray_tracer.scene_entities import Scene
+
+env_path = r"C:\dev\graphics\ray_tracer\scenes\Pool.txt"
+out_path = r"C:\dev\graphics\ray_tracer\scenes\Pool_test.png"
+scene = Scene(env_path, out_path)
--- ray_tracer/scene_entities.py
+++ ray_tracer/scene_entities.py
@@ -1,40 +1,44 @@
 
+
 # General setting for a scene
 class Sett:
 	def __init__(self, params):
-		self.bg_r = float(params[0]) # background colors (r,g,b)
+		self.bg_r = float(params[0])  # background colors (r,g,b)
 		self.bg_g = float(params[1])
 		self.bg_b = float(params[2])
-		self.shadow_rays = float(params[3]) # (N = root number of shadow rays)
-		self.rec_max = float(params[4]) # max number of recursions
+		self.shadow_rays = float(params[3])  # (N = root number of shadow rays)
+		self.rec_max = float(params[4])  # max number of recursions
 
-class Cam:
+
+class Camera:
 	def __init__(self, params):
-		self.pos_x = float(params[0]) # camera position (x,y,z)
-		self.pos_y= float(params[1])
+		self.pos_x = float(params[0])  # camera position (x,y,z)
+		self.pos_y = float(params[1])
 		self.pos_z = float(params[2])
-		self.look_x = float(params[3]) # look at position (lx,ly,lz)
+		self.look_x = float(params[3])  # look at position (lx,ly,lz)
 		self.look_y = float(params[4])
 		self.look_z = float(params[5])
-		self.up_x = float(params[6]) # up vector (ux,uy,uz)
+		self.up_x = float(params[6])  # up vector (ux,uy,uz)
 		self.up_y = float(params[7])
 		self.up_z = float(params[8]) 
 		self.sc_dist = float(params[9])
 		self.sc_width = float(params[10])
-		self.fisheye = params[11] == '1' # use fisheye if true o.w. use pinhole
-		self.k_val = float(params[12]) if len(params) > 12 else 0.5 # optional
+		self.fisheye = params[11] == '1'  # use fisheye if true o.w. use pinhole
+		self.k_val = float(params[12]) if len(params) > 12 else 0.5  # optional
+
 
 class Plane:
 	def __init__(self, params):
-		self.nx = float(params[0]) # normal vector (nx,ny,nz)
+		self.nx = float(params[0])  # normal vector (nx,ny,nz)
 		self.ny = float(params[1])
 		self.nz = float(params[2])
 		self.offset = float(params[3])
 		self.mat_ind = int(params[4])
 
+
 class Sphere:
 	def __init__(self, params):
-		self.cx = float(params[0]) # center point (cx,cy,cz)
+		self.cx = float(params[0])  # center point (cx,cy,cz)
 		self.cy = float(params[1])
 		self.cz = float(params[2])
 		self.radius = float(params[3])
@@ -43,29 +47,48 @@ class Sphere:
 	def get_material(self, scene):
 		return scene.materials[self.mat_ind]
 
+
 class Box:
 	def __init__(self, params):
-		self.pos_x = float(params[0]) # position of center point (cx,cy,cz)
+		self.pos_x = float(params[0])  # position of center point (cx,cy,cz)
 		self.pos_y = float(params[1])
 		self.pos_z = float(params[2])
 		self.edge = float(params[3])
 		self.mat_ind = int(params[4])
+
 
 class Light:
 	def __init__(self, params):
 		self.pos_x = float(params[0])
 		self.pos_y = float(params[1])
 		self.pos_z = float(params[2])
-		self.color_r = int(params[3]) 
-		self.color_g = int(params[4])
-		self.color_b = int(arams[5])
-		self.spec = float(arams[6]) # specular intensity
-		self.shadow = float(params[7]) # shadow intensity
-		self.width = float(params[8]) # light width or radius (used for soft shadow)
+		self.color_r = float(params[3])
+		self.color_g = float(params[4])
+		self.color_b = float(params[5])
+		self.spec = float(params[6])  # specular intensity
+		self.shadow = float(params[7])  # shadow intensity
+		self.width = float(params[8])  # light width or radius (used for soft shadow)
+
+
+class Material:
+	def __init__(self, params):
+		self.dr = float(params[0])
+		self.dg = float(params[1])
+		self.db = float(params[2])
+		self.sr = float(params[3])
+		self.sg = float(params[4])
+		self.sb = float(params[5])
+		self.rr = float(params[6])
+		self.rg = float(params[7])
+		self.rb = float(params[8])
+		self.phong = int(params[9])
+		self.trans = float(params[10])
+
 
 def line_to_params(line):
 	line = line.strip()
-	return line.split(' ').remove(' ')
+	return line.split()
+
 
 class Scene:
 	def __init__(self, scene_file, scene_out):
@@ -75,6 +98,8 @@ class Scene:
 		self.lights = []
 		self.materials = []
 		self.scene_out = scene_out
+		self.camera = None
+		self.sett = None
 		self.parse(scene_file)
 
 	def parse(self, scene_file):
@@ -83,13 +108,13 @@ class Scene:
 		lines = f.readlines()
 		for line in lines:
 			print(line)
-			if line.strip() == "":
-				continue
 			params = line_to_params(line)
+			if len(params) == 0:
+				continue
 			if params[0] == '#':
 				continue
 			if params[0] == 'cam':
-				self.cam = Camera(params[1:])
+				self.camera = Camera(params[1:])
 			elif params[0] == 'set':
 				self.sett = Sett(params[1:])
 			elif params[0] == 'mtl':
@@ -104,3 +129,10 @@ class Scene:
 				self.lights.append(Light(params[1:]))
 			else:
 				print("Bad param {}".format(params[0]))
+
+
+if "__main__":
+	env_path = r"C:\dev\graphics\ray_tracer\scenes\Pool.txt"
+	out_path = r"C:\dev\graphics\ray_tracer\scenes\Pool_test.png"
+	scene_object = Scene(env_path, out_path)
+	print('done')
