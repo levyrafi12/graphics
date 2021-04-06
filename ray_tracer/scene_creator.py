fb42e362c0b91c946307fd81ee3211323b010370
levyrafi12 <raffilevy@gmail.com>
2021-04-06 04:11:15 +0300

    adding some code

--- ray_tracer/scene_creator.py
+++ ray_tracer/scene_creator.py
@@ -6,19 +6,51 @@ def normalize_vector(vector):
     return vector / np.linalg.norm(vector)
 
 
+# Rotate matrix from world coord to view coord
+def set_rotate_mat(toward_vec, camera):
+    [a,b,c] = toward_vec
+    Sx = -b
+    Cx = np.sqrt(1 - Sx * Sx)
+    Sy = -a / Cx
+    Cy = c / Cx
+    M = np.zeros((3,3))
+    M[0] = [Cy, 0, Sy]
+    M[1] = [-Sx * Sy, Cx, Sx * Cy]
+    M[2] = [-Cx * Cy, -Sx, Cx * Cy]
+    return M
+
+def rotate_to_view_coord(M, toward_vec):
+    Vx = np.matmul(np.array([1,0,0]), M)
+    Vy = np.matmul(np.array([0,1,0]), M)
+    Vz = np.matmul(np.array([0,0,1]), M)
+
+    assert np.linalg.norm(toward_vec - Vz) <= 0.0001
+    return Vx, Vy, Vz
+
 def ray_casting(scene: Scene, width=500, height=500):
     camera = scene.camera
-    towards_vector = normalize_vector(camera.look_at_3d - camera.pos_3d)
+    # Tomer: towards_vector = normalize_vector(camera.look_at_3d - camera.pos_3d)
+    # https://www.cs.tau.ac.il/~dcor/Graphics/cg-slides/view04.pdf (slide 3)
+    # (P0 - P) / |P0 - P| where P0 is the eye/camera point and P is the look-at point
+    towards_vector = normalize_vector(camera.pos_3d - camera.look_at_3d)
     screen_center_point = camera.pos_3d + camera.sc_dist * towards_vector
 
-    right_vector = np.cross(towards_vector, camera.up_3d)
+    right_vector = np.cross(towards_vector, camera.up_3d) # the U vector 
     # init RGB screen
     screen = np.zeros((height, width, 3))
-    for i in range(width):
-        for j in range(height):
+    # define matrix M
+    M = set_rotate_mat(camera, toward_vector)
+    # rotate from world coord to view coord
+    Vx, Vy, _ = rotate_to_view_coord(M, toward_vector)
+    # set screen original point
+    P0 = screen_center_point - width * Vx - height * Vy   
+
+    for i in range(height):
+        p = P0
+        for j in range(width):
             pixel = np.array([i, j, 0])
             camera_position = np.array([camera.pos_x, camera.pos_y, camera.pos_z])
-
+            ray = camera_position, p - camera_position
             # ray = create_ray(camera, i, j)
             # intersection = find_intersection(ray, scene)
             # color = get_color(intersection)
