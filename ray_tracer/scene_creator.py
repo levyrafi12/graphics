6b8754d3ca3f78de45fa5de03f5f82b30884347c
levyrafi12 <raffilevy@gmail.com>
2021-04-14 20:35:02 +0300

    soft shadow ; plane intersection

--- ray_tracer/scene_creator.py
+++ ray_tracer/scene_creator.py
@@ -1,18 +1,75 @@
 import math
 
 import matplotlib.pyplot as plt
-from ray_tracer.scene_entities import Scene, Camera
+from scene_entities import Scene, Camera
 import numpy as np
-
+import random
 
 def normalize_vector(vector):
     return vector / np.linalg.norm(vector)
 
+def find_nearest_object(intersections):
+    if len(intersections) == 0:
+        return None
+
+    return min(intersections, key=lambda t: t[0])
+
+def perpendicular_plane_to_ray(point, normal):
+    D = -np.dot(point, normal)
+    return normal[0], normal[1], normal[2], D
+   
+def soft_shadow(intersection_object, scene):
+    intersect_point = intersection_object[1]
+
+    N = scene.sett.shadow_rays
+    light_intensity = 0
+    for light in scene.lights:
+        L = normalize_vector(intersect_point - light.pos_3d)
+        A,B,C,D = perpendicular_plane_to_ray(light.pos_3d, L)
+        cell_edge = light.width / N
+        half_width = light.width / 2
+        square_origin_x = light.pos_3d[0] - half_width
+        square_origin_y = light.pos_3d[1] - half_width
+        num_hits = 0
+        P_y = square_origin_y - cell_edge
+
+        for i in range(N):
+            P_x = square_origin_x - cell_edge
+            P_y += cell_edge + random.uniform(0, cell_edge)
+            for j in range(N):
+                P_x += cell_edge + random.uniform(0, cell_edge)
+                P_z = (-D - A * P_x - B * P_y) / C
+                P = np.array([P_x, P_y, P_z])
+                ray = normalize_vector(intersect_point - P)
+                intersections = find_intersections(P, ray, scene)
+                object = find_nearest_object(intersections)
+                if object == None:
+                    continue
+                if object[2] == intersection_object[2]:
+                    num_hits += 1
+
+        hit_ratio = num_hits / (N * N)
+        light_intensity += (1 - light.shadow) + light.shadow * hit_ratio
+
+        return light_intensity
+
+def find_plane_intersections(ray_origin, ray_direction, scene: Scene):
+    intersections = []
+
+    for plane in scene.planes:
+        N = plane.normal_3d
+        d = -plane.offset
+        t = -(np.dot(ray_origin, N) + d) / np.dot(ray_direction, N)
+        intersection_point = ray_origin + t * ray_direction
+        intersections.append((t, intersection_point, plane))
+
+    return intersections
 
 def find_intersections2(ray_origin, ray_direction, scene: Scene):
     intersections = []
 
     for sphere in scene.spheres:
+        # geometric method
         L = sphere.center_3d - ray_origin
         t_ca = np.dot(L, ray_direction)
         if t_ca < 0:
@@ -25,17 +82,23 @@ def find_intersections2(ray_origin, ray_direction, scene: Scene):
             continue  # the intersection is outside of the sphere
 
         t_hc = math.sqrt(r_power2 - d_power2)
-        intersection_point = min(t_ca - t_hc, t_ca + t_hc)
+        t = min(t_ca - t_hc, t_ca + t_hc) # distance
+        intersection_point = ray_origin + t * ray_direction
 
-        intersections.append([intersection_point, sphere])
+        intersections.append((t, intersection_point, sphere))
+
+    plane_intersects = find_plane_intersections(ray_origin, ray_direction, scene)
+    intersections += plane_intersects            
 
     return intersections
 
-
+# ray_origin + t * ray_direction (P = P0 + t * V)
 def find_intersections(ray_origin, ray_direction, scene: Scene):
     intersections = []
 
+    found_sphere = False
     for sphere in scene.spheres:
+        # algebric method
         L = ray_origin - sphere.center_3d
         b = 2 * np.dot(ray_direction, L)
 
@@ -43,24 +106,32 @@ def find_intersections(ray_origin, ray_direction, scene: Scene):
 
         delta = b ** 2 - 4 * c
         if delta > 0:
-            t1 = (-b + np.sqrt(delta)) / 2
-            t2 = (-b - np.sqrt(delta)) / 2
+            delta_sqrt = np.sqrt(delta)
+            t1 = (-b + delta_sqrt) / 2
+            t2 = (-b - delta_sqrt) / 2
             if t1 > 0 and t2 > 0:
-                intersections.append([min(t1, t2), sphere])
+                t = min(t1, t2) # distance
+                intersection_point = ray_origin + t * ray_direction
+                intersections.append((t, intersection_point, sphere))
+                found_sphere = True
+
+    plane_intersects = find_plane_intersections(ray_origin, ray_direction, scene)
+    intersections += plane_intersects
 
     return intersections
 
-
 def get_color(intersections, scene):
 
-    if len(intersections) == 0:
+    intersection_object = find_nearest_object(intersections)
+    if intersection_object == None:
         return np.array([0, 0, 0])  # return black
 
-    intersection_object = min(intersections, key=lambda t: t[0])
-    material = intersection_object[1].get_material(scene)
+    material = intersection_object[2].get_material(scene)
 
-    return material.difuse_color
+    light_intensity = 1
+    light_intensity = soft_shadow(intersection_object, scene)
 
+    return material.difuse_color * light_intensity
 
 # Rotate matrix from world coord to view coord
 def set_rotate_mat(toward_vec):
@@ -81,7 +152,6 @@ def rotate_to_view_coord(M):
     Vz = np.matmul(np.array([0,0,1]), M)
 
     return Vx, Vy, Vz
-
 
 def ray_casting(scene: Scene, image_width=500, image_height=500):
     camera = scene.camera
@@ -119,8 +189,8 @@ def ray_casting(scene: Scene, image_width=500, image_height=500):
     plt.imshow(screen)
     plt.show()
 
-env_path = r"C:\dev\graphics\ray_tracer\scenes\Pool.txt"
-out_path = r"C:\dev\graphics\ray_tracer\scenes\Pool_test.png"
+env_path = r"scenes\Pool.txt"
+out_path = r"scenes\Pool_test.png"
 scene = Scene(env_path, out_path)
 ray_casting(scene)
 
--- ray_tracer/scene_entities.py
+++ ray_tracer/scene_entities.py
@@ -9,7 +9,7 @@ class Sett:
 		self.bg_r = float(params[0])  # background colors (r,g,b)
 		self.bg_g = float(params[1])
 		self.bg_b = float(params[2])
-		self.shadow_rays = float(params[3])  # (N = root number of shadow rays)
+		self.shadow_rays = int(params[3])  # (N = root number of shadow rays)
 		self.rec_max = float(params[4])  # max number of recursions
 
 
@@ -41,8 +41,14 @@ class Plane:
 		self.ny = float(params[1])
 		self.nz = float(params[2])
 		self.offset = float(params[3])
-		self.mat_ind = int(params[4])
+		self.mat_ind = int(params[4]) - 1
 
+	@property
+	def normal_3d(self):
+		return np.array([self.nx, self.ny, self.nz])
+
+	def get_material(self, scene):
+		return scene.materials[self.mat_ind]
 
 class Sphere:
 	def __init__(self, params):
@@ -81,6 +87,14 @@ class Light:
 		self.shadow = float(params[7])  # shadow intensity
 		self.width = float(params[8])  # light width or radius (used for soft shadow)
 
+	@property
+	def pos_3d(self):
+		return np.array([self.pos_x, self.pos_y, self.pos_z])
+
+	@property
+	def color_3d(self):
+		return np.array([self.color_r, self.color_g, self.color_b])
+
 
 class Material:
 	def __init__(self, params):
@@ -118,11 +132,11 @@ class Scene:
 		self.parse(scene_file)
 
 	def parse(self, scene_file):
-		print(scene_file)
+		# print(scene_file)
 		f = open(scene_file, "r")
 		lines = f.readlines()
 		for line in lines:
-			print(line)
+			# print(line)
 			params = line_to_params(line)
 			if len(params) == 0:
 				continue
@@ -146,10 +160,8 @@ class Scene:
 				print("Bad param {}".format(params[0]))
 
 
-if "__main__":
-	env_path = r"C:\dev\graphics\ray_tracer\scenes\Pool.txt"
-	out_path = r"C:\dev\graphics\ray_tracer\scenes\Pool_test.png"
-	# env_path = r"scenes\Pool.txt"
-	# out_path = r"scenes\Pool_test.png"
+if __name__ == "__main__":
+	env_path = r"scenes\Pool.txt"
+	out_path = r"Pool_test.png"
 	scene_object = Scene(env_path, out_path)
 	print('done')
